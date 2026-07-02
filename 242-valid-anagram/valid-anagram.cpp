9e8ab1b16794cb11347811dea213c789022a31dc
prerika25 <prerikagarg89@gmail.com>
2026-07-02 12:18:42 +0530

    Time: 4 ms (35.03%) | Memory: 9.5 MB (99.32%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char,int>mpp1;
-        map<char,int>mpp2;
-        for(int i=0;i<s.length();i++){
-            mpp1[s[i]]++;
-        }
-        for(int j=0;j<t.length();j++){
-            mpp2[t[j]]++;
-        }
-        if(mpp1==mpp2){
-            return true;
-        }
+        if(s.length()!=t.length()) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t) return true;
         return false;
     }
 };
