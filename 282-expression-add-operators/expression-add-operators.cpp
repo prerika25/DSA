fbe8ffd1cfe1aa54ccae7f4b1c748ac105904de7
prerika25 <prerikagarg89@gmail.com>
2026-07-15 18:40:43 +0530

    Time: 399 ms (67.85%) | Memory: 92.4 MB (76.96%) - LeetSync

---
+++ 282-expression-add-operators/expression-add-operators.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfs(int index,string s,int target,vector<string> &ans,string temp,long long prev,long long res){
+        if(index==s.size()){
+            if(res==target){
+                ans.push_back(temp);
+                return;
+            }
+        }
+        string st="";
+        long long curr=0;
+        for(int j=index;j<s.size();j++){
+            if(j>index && s[index]=='0') break;
+            st+=s[j];
+            curr=curr*10+(s[j]-'0');
+            if(index==0){
+                dfs(j+1,s,target,ans,temp+st,curr,curr);
+            }
+            else{
+                dfs(j+1,s,target,ans,temp+"+"+st,curr,curr+res);
+                dfs(j+1,s,target,ans,temp+"-"+st,-curr,res-curr);
+                dfs(j+1,s,target,ans,temp+"*"+st,prev*curr,res-prev+prev*curr);
+            }
+        }
+    }
+    vector<string> addOperators(string s, int target) {
+        vector<string>ans;
+        string temp="";
+        long long prev=0;
+        dfs(0,s,target,ans,"",prev,0);
+        return ans;
+    }
+};
