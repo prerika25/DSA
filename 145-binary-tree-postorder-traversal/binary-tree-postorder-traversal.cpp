81e500b66eddc58d8202a621f8920b43b6fdcf01
prerika25 <prerikagarg89@gmail.com>
2026-08-05 19:46:56 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (17.27%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -13,8 +13,7 @@ class Solution {
 public:
     void helper(TreeNode* root,vector<int>&ans){
         if(root==NULL) return;
-        helper(root->left,ans);
-        
+        helper(root->left,ans);       
         helper(root->right,ans);
         ans.push_back(root->val);
     }
